1fa0040122babbef4f1d9c8d0b6c1caeb86afd43
jeonginsoo <macbookair13@jeong-insoo-Macbook-Air.local>
2020-04-11 04:33:38 +0900

    20200410_modify_1

--- 20200406_4주차/diamondStar.js
+++ 20200406_4주차/diamondStar.js
@@ -1,4 +1,14 @@
-var floor = prompt("몇 층으로 만들까요?", 0);
+var floor = prompt("몇 층으로 만들까요?");
+floor = parseInt(floor);
+
+var IMG =
+  "http://postfiles13.naver.net/20160309_220/hwee___1457528460795IuUC7_JPEG/P20160302_130332000_A603EF99-8AC1-4432-BCEA-087031CB7606.JPG?type=w1";
+
+if (floor === 0) {
+  document.write(`<img src=${IMG} loading=lazy/>`);
+  document.write("<h1>?!?!?!?!?!?!!!?!?!?!!?!!!</h1>");
+}
+
 var star = 0;
 var space = 0;
 
@@ -13,16 +23,15 @@ var rainbow = [
   "",
 ];
 var cnt = 0;
-floor = parseInt(floor);
 
 star = floor * 2;
-space = star / 2;
+space = floor;
 
 for (var i = 0; i < floor; i++) {
   if (cnt === 7) {
     cnt = 0;
   }
-  /* 공백처리 (center 태그로 해결됨)
+  /* 공백처리 (center 태그로 대체)
   for (var j = 1; j < space; j++) {
     document.write("&nbsp&nbsp&nbsp");
   }
@@ -36,13 +45,13 @@ for (var i = 0; i < floor; i++) {
   space--;
   cnt++;
 }
-space = star / 2;
+space = floor;
 
 for (var i = 1; i < floor; i++) {
   if (cnt === 7) {
     cnt = 0;
   }
-  /* 공백처리 (center 태그로 해결됨)
+  /* 공백처리 (center 태그로 대체)
   for (var j = 0; j <= floor - space; j++) {
     document.write("&nbsp&nbsp&nbsp");
   }
