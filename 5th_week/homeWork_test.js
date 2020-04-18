bfbe3e8130ab0633230bf8890c004a64dc2fc987
jeonginsoo <macbookair13@jeong-insoo-Macbook-Air.local>
2020-04-18 21:34:52 +0900

    20200418

---
+++ 5th_week/homeWork_test.js
@@ -0,0 +1,25 @@
+var arr = [];
+var num = 0;
+
+// 배열 초기화 (1 ~ 10)
+for (var i = 1; i <= 10; i++) {
+  arr[i] = i;
+}
+
+// 무한루프에서 빠져나오기위한 변수
+var chk = 0;
+
+while (true) {
+  num = Math.ceil(Math.random() * 10);
+  if (arr[num] == num) {
+    document.write(arr[num], "<br>");
+    arr[num] = null;
+    chk++;
+  } else {
+    num = Math.ceil(Math.random() * 10);
+  }
+
+  if (chk == 10) {
+    break;
+  }
+}
